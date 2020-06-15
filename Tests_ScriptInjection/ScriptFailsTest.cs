8ff3f4bd9eb9f584a0d23e6410ecdf19f0bec379
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-06-15 13:50:46 +0200

    added test

---
+++ Tests_ScriptInjection/ScriptFailsTest.cs
@@ -0,0 +1,16 @@
+﻿using Bunit;
+using Excubo.Blazor.ScriptInjection;
+using NUnit.Framework;
+using System;
+
+namespace Excubo.Blazor.Tests_ScriptInjection
+{
+    public class ScriptFailsTest : ComponentTestFixture
+    {
+        [Test]
+        public void FailWithoutScripts()
+        {
+            Assert.Throws<InvalidOperationException>(() => RenderComponent<Script>());
+        }
+    }
+}
