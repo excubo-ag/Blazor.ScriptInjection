88e2a83a1de86c077556db4552a60ebcb4796aee
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2021-08-11 12:24:19 +0200

    [deploy] updated packages

--- ScriptInjection/ServiceExtension.cs
+++ ScriptInjection/ServiceExtension.cs
@@ -1,17 +1,10 @@
-﻿using Excubo.Analyzers.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.JSInterop;
 
 namespace Excubo.Blazor.ScriptInjection
 {
     public static class ServiceExtension
     {
-        [Exposes(typeof(ScriptInjectionTracker)), As(typeof(IScriptInjectionTracker))]
-#pragma warning disable CS0618 // Type or member is obsolete
-        [Exposes(typeof(Script))]
-#pragma warning restore CS0618 // Type or member is obsolete
-        [Exposes(typeof(AddScript))]
-        [IgnoreDependency(typeof(IJSRuntime))]
         public static IServiceCollection AddScriptInjection(this IServiceCollection services, bool onload_notification = true, bool gzipped_bootstrap = false)
         {
             return services.AddScoped<IScriptInjectionTracker>((sp) => new ScriptInjectionTracker(onload_notification, gzipped_bootstrap));
